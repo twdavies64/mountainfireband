5f7fb437ec61635374b812e50bb5aa62ed7db3fa
Tom Davies <twdavies64@gmail.com>
2025-11-04 22:16:13 -0800

    Initial commit: Mountain Fire Band site

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+﻿// This next section sets up the fading images for the features section on the sidebar. 
+// SPEED: In milliseconds - how long the transition should take.
+// TYPE: Type of slideshow: 'sequence', 'random' or 'random_start'
+// CONTAINERHEIGHT: The height of the div that is calling the innerfade method.
+
+
+$(function() {
+    $('div#featured').innerfade({
+        speed: 1500,
+        timeout: 9000,
+        type: 'sequence',
+        containerheight: '729px'
+    });
+
+});
